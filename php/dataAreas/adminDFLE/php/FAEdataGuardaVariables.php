e6d254fc26fdd05cf0d279904e937d788776d600
Oscar Arturo Pérez Solano <osscar.a.p.solano@gmail.com>
2024-06-24 03:53:33 -0600

    FAE

--- php/dataAreas/adminDFLE/php/FAEdataGuardaVariables.php
+++ php/dataAreas/adminDFLE/php/FAEdataGuardaVariables.php
@@ -1,5 +1,16 @@
 <?php
 
 var_dump($_POST);
-
+$datosVaraibles = ' 
+    INSERT INTO FAES_DFLE_Indicadores(
+    Clave_Indicador, 
+    Variable, 
+    PRIMER_TRIM, 
+    SEGUNDO_TRIM, 
+    TERCER_TRIM, 
+    CUARTO_TRIM, 
+    Fecha)
+    VALUES(?, ?, ?, ?, ?, ?, ?)
+';
+$fecha = date('d-m-Y H:i:s');
 ?>
