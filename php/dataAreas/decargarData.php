10717cc7da7ac3effce950a104705a2726886eb7
Oscar Arturo Pérez Solano <osscar.a.p.solano@gmail.com>
2024-07-01 14:41:15 -0600

    formatos sombras

--- php/dataAreas/decargarData.php
+++ php/dataAreas/decargarData.php
@@ -8,30 +8,62 @@ if (!isset($_SESSION['correo'])) {
 $correo = $_SESSION['correo'];
 $nombre_usuario = $_SESSION['nombre_usuario'];
 
-function archivoExistencia($nombre) {
-    $rutaData = '../exelDFLE/unidades/' . $nombre . '.xlsx';
-    return file_exists($rutaData);
+// Directorio de archivos
+$ficheroArchivos = '../exelDFLE/unidades';
+
+// Verificar si el directorio existe
+if (!is_dir($ficheroArchivos)) {
+    die("El directorio no existe.");
 }
 
-// Ruta donde se guardará la copia del archivo
-$nombreArchivo = 'General_'.$nombre_usuario;
-$rutaCopiaArchivo = '../exelDFLE/unidades/'.$nombreArchivo.'.xlsx';
+// Año actual
+$anio = (string)date('Y');
 
-if (archivoExistencia($nombreArchivo)) {
+// Arrays para clasificar los archivos
+$archivos4T = [];
+
+// Abrir el directorio
+if ($handle = opendir($ficheroArchivos)) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            if (strpos($entry, "4 DFLE_") !== false) {
+                $archivos4T[] = $entry;
+            }
+        }
+    }
+    closedir($handle);
+} else {
+    die("No se pudo abrir el directorio.");
+}
+
+// Buscar archivo específico
+$mes = date('n');
+$numTrimestre = match (true) {
+    $mes <= 3 => 1,
+    $mes <= 6 => 2,
+    $mes <= 9 => 3,
+    $mes <= 12 => 4,
+    default => "Mes inválido"
+};
+$nombreArchivoBuscado = '4 DFLE_' . $numTrimestre . 'T_' . $anio . ' ' . $nombre_usuario . '.xlsx';
+
+if (in_array($nombreArchivoBuscado, $archivos4T)) {
+    $rutaArchivo = $ficheroArchivos . '/' . $nombreArchivoBuscado;
+
     // Establecer las cabeceras para la descarga del archivo
     header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'. basename($rutaCopiaArchivo) . '"');
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment; filename="'. basename($rutaArchivo) . '"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
-    header('Content-Length: ' . filesize($rutaCopiaArchivo));
+    header('Content-Length: ' . filesize($rutaArchivo));
 
     // Enviar el contenido del archivo al navegador
-    readfile($rutaCopiaArchivo);
+    readfile($rutaArchivo);
 
-    // Redirigir después de enviar el archivo
-    header("Location: cargarData.php?status=ArchivoDownload");
+    // Añadir script para redirigir después de la descarga
+    echo '<script type="text/javascript">window.location.href = "cargarData.php?status=ArchivoDescargado";</script>';
     exit();
 } else {
     // Redirigir si el archivo no existe
